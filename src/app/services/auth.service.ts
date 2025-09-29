bde8de8a58b41ca473265fa2cfbea9565b093132
ni3github <ni3deshmukh96@gmail.com>
2025-09-29 12:21:58 +0530

    bv

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -35,7 +35,7 @@ export interface AuthResponse {
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://angecombackend.onrender.com/api/auth';
+  private apiUrl = 'http://localhost:8082/api/auth';
   private tokenKey = 'auth_token';
   private userKey = 'current_user';
   
